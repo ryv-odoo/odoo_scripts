b2596247598565220abfd0f132fd7e8af3173790
Rémy Voet (ryv) <ryv@odoo.com>
2022-04-14 10:15:37 +0200

    unaccnet test

--- test_indexes.py
+++ test_indexes.py
@@ -23,47 +23,81 @@ def create_gin_index(conn):
             CREATE INDEX {TABLE}_gin ON {TABLE} USING gin(name gin_trgm_ops);
         """)
 
+def create_o_unaccent_gin_index(conn):
+    with conn.cursor() as cur:
+        cur.execute(f"""
+            CREATE INDEX {TABLE}_gin_unaccent ON {TABLE} USING gin(o_unaccent(name) gin_trgm_ops);
+        """)
+
+def create_index(conn):
+    with conn.cursor() as cur:
+        cur.execute(f"""
+            CREATE INDEX {TABLE}_index ON {TABLE} USING (name);
+        """)
+
 
 def create_row(conn, todo):
     with conn.cursor() as cur:
         cur.execute(f"""
         INSERT INTO {TABLE} (name)
         SELECT
-            'bla' || s::text
+            repeat(md5(s::text), 40)
         FROM generate_series(1, {todo}) AS s
         """)
     conn.commit()
 
 psql_activate_trigram(CONNECTION_PARAMS)
 psql_activate_unaccent(CONNECTION_PARAMS)
-psql_set_timeout(CONNECTION_PARAMS, 20)
+psql_set_timeout(CONNECTION_PARAMS, 60)
 
 with psycopg2.connect(CONNECTION_PARAMS) as conn:
     timed_call(create_table, conn)
-    timed_call(create_row, conn, 1_000_000)
+    timed_call(create_row, conn, 1_000_00)
 
 timed_call(psql_vacuum_analyse, CONNECTION_PARAMS, TABLE)
 
+"""
+CREATE OR REPLACE FUNCTION public.o_unaccent(text)
+RETURNS text AS 'SELECT unaccent($1);'
+LANGUAGE SQL IMMUTABLE;
+
+SELECT unaccent('unaccent', $1)
+$func$ LANGUAGE sql IMMUTABLE;
+
+"""
+
+
+"""
+CREATE SCHEMA IF NOT EXISTS unaccent_schema;
+CREATE EXTENSION IF NOT EXISTS unaccent SCHEMA unaccent_schema;
+CREATE OR REPLACE FUNCTION public.o_unaccent(text)
+    RETURNS text AS
+$func$
+SELECT unaccent_schema.unaccent('unaccent_schema.unaccent', $1)
+$func$ LANGUAGE sql IMMUTABLE;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA unaccent_schema TO public;
+GRANT USAGE ON SCHEMA unaccent_schema TO public;
+"""
 
 def tests(conn):
     with conn.cursor() as cur:
         print("Normal ilike:")
         query_normal = f"""
-        SELECT id, name FROM {TABLE} WHERE name ILIKE '%bla25%'
+        SELECT 1 FROM {TABLE} WHERE name ILIKE '%bla25%'
         """
         psql_explain_analyse(cur, query_normal)  # warmup
         print(psql_explain_analyse(cur, query_normal))
 
         print("\nUnaccent ilike:")
         query_unaccent = f"""
-        SELECT id, name FROM {TABLE} WHERE unaccent(name) ILIKE unaccent('%bla25%')
+        SELECT 1 FROM {TABLE} WHERE unaccent(name) ILIKE unaccent('%a8a6%')
         """
         psql_explain_analyse(cur, query_unaccent)  # warmup
         print(psql_explain_analyse(cur, query_unaccent))
 
-        print("\nUnaccent ilike:")
+        print("\nO_Unaccent ilike:")
         query_unaccent = f"""
-        SELECT id, name FROM {TABLE} WHERE unaccent(name) COALESCE unaccent('%bla25%')
+        SELECT 1 FROM {TABLE} WHERE o_unaccent(name) ILIKE o_unaccent('%a8a6%')
         """
         psql_explain_analyse(cur, query_unaccent)  # warmup
         print(psql_explain_analyse(cur, query_unaccent))
@@ -73,7 +107,6 @@ print("WITHOUT INDEX")
 with psycopg2.connect(CONNECTION_PARAMS) as conn:
     timed_call(tests, conn, suffix='without index')
 
-print()
 with psycopg2.connect(CONNECTION_PARAMS) as conn:
     timed_call(create_gin_index, conn)
 timed_call(psql_vacuum_analyse, CONNECTION_PARAMS, TABLE)
@@ -83,8 +116,14 @@ print("WITH GIN INDEX")
 with psycopg2.connect(CONNECTION_PARAMS) as conn:
     timed_call(tests, conn, suffix='with index')
 
+with psycopg2.connect(CONNECTION_PARAMS) as conn:
+    timed_call(create_o_unaccent_gin_index, conn)
+timed_call(psql_vacuum_analyse, CONNECTION_PARAMS, TABLE)
 
-
+print()
+print("WITH Unaccent GIN INDEX")
+with psycopg2.connect(CONNECTION_PARAMS) as conn:
+    timed_call(tests, conn, suffix='with Unaccent index')
 # COALESCE("ir_translation"."value", "model"."field") ilike '%pattern%'
 
 # ("ir_translation"."value" IS NOT NULL and "ir_translation"."value" ilike '%pattern%') OR ("ir_translation"."value" IS NULL and "model"."field" ilike '%pattern%')
