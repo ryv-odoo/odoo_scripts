bb0d73cab4c7a02e4cfb6716fab5e93e452ae6d0
Rémy Voet (ryv) <ryv@odoo.com>
2022-07-11 13:44:09 +0200

    test_website fix

--- test_website_perf.py
+++ test_website_perf.py
@@ -1,23 +1,22 @@
-import random
 import grequests
 import time
 
 urls = [
-    "http://127.0.0.1:8069/",
-    "http://127.0.0.1:8069/shop",
-    "http://127.0.0.1:8069/blog",
-    "http://127.0.0.1:8069/event",
+    "http://127.0.0.1:8069/",  # 9 t-cache get
+    "http://127.0.0.1:8069/shop",  # 35 t-cache get
+    "http://127.0.0.1:8069/blog",  # 25 t-cache get
+    "http://127.0.0.1:8069/event",  # 21 t-cache get
 ]
 
-random.seed(5)
+parallel_request = 2
 
 request_getters = [grequests.get(urls[i % len(urls)]) for i in range(12)]
 
 s = time.time_ns()
-response = grequests.map(request_getters, size=4)
+response = grequests.map(request_getters, size=parallel_request)
 print(f"Warmup (12 requests) takes {((time.time_ns() - s) / 1_000_000_000):.4f} sec")
 
-request_getters = [grequests.get(urls[random.randint(0, len(urls) - 1)]) for _ in range(200)]
+request_getters = [grequests.get(urls[i % len(urls)]) for i in range(500)]
 s = time.time_ns()
-response = grequests.map(request_getters, size=4)
+response = grequests.map(request_getters, size=parallel_request)
 print(f"200 requests takes {((time.time_ns() - s) / 1_000_000_000):.4f} sec")
