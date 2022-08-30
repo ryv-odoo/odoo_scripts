1ea674f34a455653c968a0cba0a29b72f7dc0925
Rémy Voet (ryv) <ryv@odoo.com>
2022-08-30 16:31:12 +0200

    small change

--- modified_trigger_perf.py
+++ modified_trigger_perf.py
@@ -1,11 +1,12 @@
 
 
 import re
+import sys
 
 from statistics import quantiles
 
-f_before = "before.log"
-f_after = "after.log"
+f_before = sys.argv[1]
+f_after = sys.argv[2]
 
 re_line = re.compile(r"modified takes (.*) ms \((.*) ms without sql time\) with (\d+) extra queries - (\d+)")
 
@@ -25,5 +26,7 @@ with open(f_before, "rt") as f1, open(f_after, "rt") as f2:
     print(f"Before tot: {sum(befores_stat[0]):.3f} ms, nosql: {sum(befores_stat[1]):.3f} ms, extra sql : {sum(befores_stat[2])}")
     print(f"After  tot: {sum(afters_stat[0]):.3f} ms, nosql: {sum(afters_stat[1]):.3f} ms, extra sql : {sum(afters_stat[2])}")
 
-    print(f"Before Decile nosql:{quantiles(befores_stat[1], n=10)}")
-    print(f"After  Decile nosql:{quantiles(afters_stat[1], n=10)}")
+    q_before = [f'{q:.3f}' for q in quantiles(befores_stat[0], n=20)]
+    q_after = [f'{q:.3f}' for q in quantiles(afters_stat[0], n=20)]
+    print(f"Before Decile nosql: {q_before} ms")
+    print(f"After  Decile nosql: {q_after} ms")
