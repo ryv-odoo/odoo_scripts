274139e12c61747526e0dbbf8c2b10ec38854863
Rémy Voet (ryv) <ryv@odoo.com>
2023-04-17 13:31:37 +0200

    test perf read_group final

--- new_read_group/parse.py
+++ new_read_group/parse.py
@@ -32,41 +32,46 @@ with open(f_before, "rt") as f1, open(f_after, "rt") as f2:
         # elif (method_1, model_1, domain_1) != (method_2, model_2, domain_2):
         #     if domain_1 != domain_2 and (('date' in domain_1 and 'date' in domain_2) or (method_1 in '_get_mo_count')):
         #         continue
+        #     if domain_1 != domain_2 and 'odoo.tools.query.Query' in domain_2:
+        #         continue
+        #     if domain_1 != domain_2 and method_1 in ('_get_task_document_data', '_compute_shared_document_ids', '_compute_last_visited_page_id'):  # Not same order domain
+        #         continue
         #     print(f"Line {i} differ {(method_1, model_1, domain_1)} != {(method_2, model_2, domain_2)}")
         #     break
+
         n_1 = NormalDist(mean_1, std_1 or 0.0000001)
         n_2 = NormalDist(mean_2, std_2 or 0.0000001)
+
+
         if 'ir.attachment' in model_1:  # Before security check was broken
             continue
+        if method_1 in ('_subscription_count'):  # Before one query before instead of subquery
+            continue
 
-        if mean_2 < mean_1:
-            # After faster
-            ratio = (mean_1 - mean_2) / mean_1
-        else:
-            ratio = - ((mean_2 - mean_1) / mean_2)
+        if n_1.overlap(n_2) < 0.05:
+            if mean_1 < mean_2:
+                ratio = - ((mean_2 - mean_1) / mean_2)
+                percentage_slower = ((mean_2 - mean_1) / mean_1) * 100
+                # print(f"Slower {percentage_slower:.4f} %", i, model_1[:30], method_1, mean_1, mean_2, ratio)
+                slowers.append(percentage_slower)
+                slowers_diff.append(mean_2 - mean_1)
+            if mean_1 > mean_2:
+                ratio = (mean_1 - mean_2) / mean_1
+                percentage_faster = ((mean_1 - mean_2) / mean_2) * 100
+                # print(f"Faster {percentage_faster:.4f} %", i, model_1[:30], method_1, mean_1, mean_2, ratio)
+                fasters.append(percentage_faster)
+                fasters_diff.append(mean_1 - mean_2)
 
-        ratios.append(ratio)
-
-        print(i, model_1[:30], method_1, mean_1, mean_2, ratio)
+            ratios.append(ratio)
 
 
-    #     if n_1.overlap(n_2) < 0.05:
-    #         if n_1.mean < n_2.mean:
-    #             percentage_slower = ((n_2.mean - n_1.mean) / n_1.mean) * 100
-    #             print(f"Slower {percentage_slower:.4f} %", i, model_1[:30], method_1, mean_1, mean_2)
-    #             slowers.append(percentage_slower)
-    #             slowers_diff.append(n_2.mean - n_1.mean)
-    #         if n_1.mean > n_2.mean:
-    #             percentage_faster = ((n_1.mean - n_2.mean) / n_2.mean) * 100
-    #             print(f"Faster {percentage_faster:.4f} %", i, model_1[:30], method_1, mean_1, mean_2)
-    #             fasters.append(percentage_faster)
-    #             fasters_diff.append(n_1.mean - n_2.mean)
-
-    # print(f"Slower {len(slowers)}/{len(matches_before)}: sum_difference = {sum(slowers_diff):.4f}, mediam % {median(slowers):.4f}, mean % {fmean(slowers):.4f}")
-    # print(f"Faster {len(fasters)}/{len(matches_before)}: sum_difference = {sum(fasters_diff):.4f}, mediam % {median(fasters):.4f}, mean % {fmean(fasters):.4f}")
     def ratio_to_time_faster(ratio):
         return (1 / (1 - ratio))
     print(f"In average new read_group is {fmean(ratios)} -> {ratio_to_time_faster(fmean(ratios))} faster")
     print(f"Median: {median(ratios)} -> {ratio_to_time_faster(median(ratios))} faster")
     print(f"Max: {max(ratios)} -> {ratio_to_time_faster(max(ratios))} faster")
     print(f"Min: {min(ratios)} -> {ratio_to_time_faster(min(ratios))} faster")
+
+    print("Cases:")
+    print(f" - Slower {len(slowers)}/{len(matches_before)}: sum_difference = {sum(slowers_diff):.4f} ms, mediam % {median(slowers):.4f}, mean % {fmean(slowers):.4f}")
+    print(f" - Faster {len(fasters)}/{len(matches_before)}: sum_difference = {sum(fasters_diff):.4f} ms, mediam % {median(fasters):.4f}, mean % {fmean(fasters):.4f}")
