b2c65e211890a16c982bb92a3861b87165680fcb
Rémy Voet (ryv) <ryv@odoo.com>
2025-09-03 12:51:02 +0200

    faker imp

--- fake_data.py
+++ fake_data.py
@@ -8,23 +8,29 @@ fake = Faker()
 
 
 with open('out_fake.csv', 'wt') as f:
-    for i in range(200_000):
-        name = fake.name()
+    for i in range(500_000):
+        if random.random() < 0.85:
+            name = fake.name()
+        else:
+            name = ''
 
-        if random.random() < 0.30:
+        if random.random() < 0.85:
             email = fake.email()
         else:
             email = ''
 
-        if random.random() < 0.40:
+        if random.random() < 0.05:
             ref = fake.ean8()
         else:
             ref = ''
 
-        if random.random() < 0.45:
+        if random.random() < 0.10:
             vat = fake.iban()
         else:
             vat = ''
+
+        if not any([name, email, ref, vat]):
+            continue
 
         line = ",".join([name, email, ref, vat])
         f.write(f"{line}\n")
