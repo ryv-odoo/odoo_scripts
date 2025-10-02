cf40cb4eb114247cf3461547bcd858e7214e9749
Rémy Voet (ryv) <ryv@odoo.com>
2025-10-02 12:42:22 +0200

    create currency rate

--- perf_currency_rate.py
+++ perf_currency_rate.py
@@ -1,40 +1,37 @@
 import random
 import datetime
 
-from itertools import product, cycle
+from itertools import product
 
-from faker import Faker
+from dateutil.relativedelta import relativedelta
 
-fake = Faker()
+# from faker import Faker
+
+# fake = Faker()
 
 # faker -r=1000 -s=',' name email ean8 vat
 
 currencies_ids = list(range(1, 8))
-company_ids = list(range(1, 10))
+company_ids = list(range(1, 10)) + ['NULL']
+# +- 10 years of data
+days = [datetime.date.today()]
+for __ in range(10 * 365):
+    days.append(days[-1] - relativedelta(days=1))
 
-iter_currencies_company = cycle(product(currencies_ids, company_ids))
-
-today = datetime.date.today()
-
-# TODO
-
+# \copy res_currency_rate(currency_id, name, company_id, rate) FROM '/home/odoo/Documents/dev/odoo_scripts/currency_rate.csv' (FORMAT csv, DELIMITER ',', NULL 'NULL');
 with open("currency_rate.csv", "wt") as f:
-    for i in range(100_000):
-
-        currency_id = next(currency_iter)
-
-        date 
-
-        line = ",".join([currency_id, date, company_id])
+    for currency_id, company_id, date in product(currencies_ids, company_ids, days):
+        rate = random.random() * 10
+        line = ",".join([str(currency_id), str(date), str(company_id), str(rate)])
         f.write(f"{line}\n")
-
 
 """
 
 SELECT DISTINCT ON ("res_currency_rate"."currency_id") 
     "res_currency_rate"."currency_id", "res_currency_rate"."rate"
 FROM "res_currency_rate"
-WHERE "res_currency_rate"."company_id" IS NULL OR "res_currency_rate"."company_id" = 1
+WHERE ("res_currency_rate"."company_id" IS NULL OR "res_currency_rate"."company_id" = 1) 
+AND "res_currency_rate"."currency_id" = 5
 ORDER BY
     "res_currency_rate"."currency_id",
     "res_currency_rate"."company_id",
@@ -78,5 +75,5 @@ SELECT
     )
 FROM
     "res_currency"
-WHERE "res_currency" = 10
+WHERE "res_currency"."id" = 5
 """
